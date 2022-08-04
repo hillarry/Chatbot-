735bf80070ca8c74c396402ad51fd290dfa91a3a
hillarry <53052353+hillarry@users.noreply.github.com>
2022-08-04 23:24:21 +0630

    Update chatbotst.py

--- chatbotst.py
+++ chatbotst.py
@@ -84,6 +84,7 @@ def get_text():
     return input_text 
 user_input = get_text()
 
-st.text_input("Talk to the bot",key='input_text',on_change=chatbot_response(user_input))
-
+#st.text_input("Talk to the bot",key='input_text',on_change=chatbot_response(user_input))
+if user_input:
+    chatbot_response(user_input)
     
